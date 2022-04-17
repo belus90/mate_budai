d5ae1d56270218ac591b61074e4779ab90aa6879
belus90 <belus90@gmail.com>
2022-04-17 22:23:44 +0100

    Updates

---
+++ precache-manifest.bc2c4e6bfc733e79b59f5b1bfc8bf7c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcbb6b23f46585d6e485",
+    "url": "/mate_budai/static/css/main.cf0f9836.chunk.css"
+  },
+  {
+    "revision": "dcbb6b23f46585d6e485",
+    "url": "/mate_budai/static/js/main.dcbb6b23.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/mate_budai/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "e495a233d13d271953c2",
+    "url": "/mate_budai/static/js/2.e495a233.chunk.js"
+  },
+  {
+    "revision": "e9b55bfd91dfb18bce64",
+    "url": "/mate_budai/static/js/runtime~main.e9b55bfd.js"
+  },
+  {
+    "revision": "5ccb94871ffab4aba1a7fbac4635543a",
+    "url": "/mate_budai/index.html"
+  }
+];

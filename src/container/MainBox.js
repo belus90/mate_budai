88fb13af32d389079d9f015cf874e302a37d73ac
belus90 <belus90@gmail.com>
2022-04-17 13:37:03 +0100

    practice 1

---
+++ src/container/MainBox.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import MainCointext from "../components/MainContext";
+
+
+const MainBox = () => {
+
+    
+
+
+
+
+
+    return (
+        <>
+        <h1>Hello</h1>
+        </>
+        
+    );
+    
+
+};
+
+
+export default MainBox;

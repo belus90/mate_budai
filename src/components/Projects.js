50cc165ca1c2d0b5bd7f054fc41a2e170f552541
belus90 <belus90@gmail.com>
2022-08-10 14:46:47 +0100

    card's basic done 2.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,32 +2,35 @@ import React from "react";
 import "../style/Projects.css";
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import project1 from "../img/Projects/BeyondTraveling.png";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const Portfolio = () => {
 
 
         return (
 
-        <div class="projects">
-        <h2>My Projects</h2>
-        <br></br>
-        <div className="projects-card">
-                <div className="projects-card-1">
+                <div class="projects">
+                        <h2>My Projects</h2>
                         <br></br>
-                        <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                                <Card.Title>Gym Project</Card.Title>
-                                <Card.Text>
-                                        Some quick example text to build on the card title and make up the
-                                        bulk of the card's content.
-                                </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                        </Card>
-                <br></br>
-                </div>
-                <div className="projects-card-2">
+                        <div className="projects-card">
+                                <div className="projects-card-1">
+                                        <br></br>
+                                        <Card border="light" style={{ width: '18rem' }}>
+                                                <Card.Img variant="top" src={project1} />
+                                                <Card.Body>
+                                                        <Card.Title>Gym Project</Card.Title>
+                                                        <Card.Text>
+                                                                Some quick example text to build on the card title and make up the
+                                                                bulk of the card's content.
+                                                        </Card.Text>
+                                                        <Button variant="primary">Go somewhere</Button>
+                                                </Card.Body>
+                                        </Card>
+                                        <br></br>
+                                </div>
+                                {/* <div className="projects-card-2">
                         <br></br>
                         <Card style={{ width: '18rem' }}>
                         <Card.Img variant="top" src="holder.js/100px180" />
@@ -71,15 +74,15 @@ const Portfolio = () => {
                         </Card.Body>
                         </Card>
                 <br></br>
-                </div>
-                        
-                
+                </div> */}
+
+
                                 {/* <a href="https://github.com/belus90/First_project">Gym project</a>
                         <a href="https://github.com/belus90/rock_paper_scissors">MSN Share Portfolio</a>
                         <a href="">Beyond travelling App</a>
                         <a href="">Extra</a> */}
-        </div>
-        </div>            
+                        </div>
+                </div>
 
         );
 

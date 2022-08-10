0ad79e05dd637dd971e188e3277a9c7be310c705
belus90 <belus90@gmail.com>
2022-08-10 14:11:57 +0100

    card's basic  done for the projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,30 +1,87 @@
 import React from "react";
 import "../style/Projects.css";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 const Portfolio = () => {
 
 
-return (
+        return (
 
-    <div class = "projects">
-    <h2>My Projects</h2>
-        <li>
-                <a href="https://github.com/belus90/First_project">First Project</a>
-        </li>
-        <li>
-                <a href="https://github.com/belus90/rock_paper_scissors">Rock Paper Scissors</a>
-        </li>
-        <li>
-                <a href="">Project1t</a>
-        </li>
-        <li>
-                <a href="">Project2</a>
-        </li>
-        <li>
-            < a href="">Project3</a>
-        </li>
-    </div>
-);
+        <div class="projects">
+        <h2>My Projects</h2>
+        <br></br>
+        <div className="projects-card">
+                <div className="projects-card-1">
+                        <br></br>
+                        <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                                <Card.Title>Gym Project</Card.Title>
+                                <Card.Text>
+                                        Some quick example text to build on the card title and make up the
+                                        bulk of the card's content.
+                                </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                        </Card>
+                <br></br>
+                </div>
+                <div className="projects-card-2">
+                        <br></br>
+                        <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                                <Card.Title>Gym Project</Card.Title>
+                                <Card.Text>
+                                        Some quick example text to build on the card title and make up the
+                                        bulk of the card's content.
+                                </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                        </Card>
+                <br></br>
+                </div>
+                <div className="projects-card-3">
+                        <br></br>
+                        <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                                <Card.Title>Gym Project</Card.Title>
+                                <Card.Text>
+                                        Some quick example text to build on the card title and make up the
+                                        bulk of the card's content.
+                                </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                        </Card>
+                <br></br>
+                </div>
+                <div className="projects-card-4">
+                        <br></br>
+                        <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                                <Card.Title>Gym Project</Card.Title>
+                                <Card.Text>
+                                        Some quick example text to build on the card title and make up the
+                                        bulk of the card's content.
+                                </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                        </Card>
+                <br></br>
+                </div>
+                        
+                
+                                {/* <a href="https://github.com/belus90/First_project">Gym project</a>
+                        <a href="https://github.com/belus90/rock_paper_scissors">MSN Share Portfolio</a>
+                        <a href="">Beyond travelling App</a>
+                        <a href="">Extra</a> */}
+        </div>
+        </div>            
+
+        );
 
 };
 
